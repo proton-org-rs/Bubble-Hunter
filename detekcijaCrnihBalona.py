bc69567e42d88e114a00d53228d97a8e9424b87c
Nikola Glavaski <nikolaglavaski05@gmail.com>
2026-08-11 16:53:07 +0200

    Update detekcijaCrnihBalona.py
    Dodat novi kriterijum koji koristi HoughCircles. Liste boxes se sada prave u svojim funkcijama, ne u spojiDetekcije

--- detekcijaCrnihBalona.py
+++ detekcijaCrnihBalona.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 Blue = (255, 0, 0)
 Green = (0, 255, 0)
 Red = (0, 0, 255)
+Pink = (255, 0, 255)
 
 def gamma_correction(img, gamma):
     inv_gamma = 1.0 / gamma
@@ -30,7 +31,20 @@ def nadjiCrno(img):
     cv2.imshow("detektovano", thresh)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
+
+    boxes = []
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area < 500:
+            continue
+        perimeter = cv2.arcLength(c, True)
+        if perimeter == 0:
+            continue
+        circularity = 4 * np.pi * (area / (perimeter ** 2))
+        if circularity > 0.01:
+            boxes.append(cv2.boundingRect(c))
+
+    return boxes
 
 
 def nadjiKonture(img):
@@ -45,8 +59,45 @@ def nadjiKonture(img):
     cv2.imshow("edges_dilated", edges_dilated)
 
     contours, hierarchy = cv2.findContours(edges_dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    return contours
 
+    boxes = []
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area < 500:
+            continue
+        perimeter = cv2.arcLength(c, True)
+        if perimeter == 0:
+            continue
+        circularity = 4 * np.pi * (area / (perimeter ** 2))
+        if circularity > 0.7:
+            boxes.append(cv2.boundingRect(c))
+
+    return boxes
+
+def nadjiKrugove(img):
+    grayFrame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blurFrame = cv2.GaussianBlur(grayFrame, (11, 11), 0)
+
+    circles = cv2.HoughCircles(blurFrame, cv2.HOUGH_GRADIENT, 1.2, 100,
+                              param1=60, param2=80, minRadius=5, maxRadius=0)
+    # param1 je sensitivity, veci broj znaci manje krugova nadje
+    # param2 je accuracy, number of edge points needed, veci broj znaci manje krugova
+    boxes = []
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            x, y, r = int(i[0]), int(i[1]), int(i[2])
+            height, width = img.shape[:2]
+
+            x1 = np.clip(x - r, 0, width)
+            y1 = np.clip(y - r, 0, height)
+            x2 = np.clip(x + r, 0, width)
+            y2 = np.clip(y + r, 0, height)
+            boxes.append((x1, y1, 2*r, 2*r))
+    return boxes
+
+def povrsina(box):
+    return box[2] * box[3]
 
 def presek(boxA, boxB):
     xA1, yA1, wA, hA = boxA
@@ -64,7 +115,7 @@ def presek(boxA, boxB):
     return (interX1, interY1, interW, interH)
 
 
-def iou(boxA, boxB):
+def iou2(boxA, boxB):
     interX1, interY1, interW, interH = presek(boxA, boxB)
     interArea = interW * interH
 
@@ -76,40 +127,37 @@ def iou(boxA, boxB):
         return 0
     return interArea / unionArea
 
+def iou3(boxA, boxB, boxC):
+    boxAB = presek(boxA, boxB)
+    return iou2(boxAB, boxC)
 
-def spojiDetekcije(contours1, contours2):
-    boxes1 = []
-    for c in contours1:
-        area = cv2.contourArea(c)
-        if area < 500:
-            continue
-        perimeter = cv2.arcLength(c, True)
-        if perimeter == 0:
-            continue
-        circularity = 4 * np.pi * (area / (perimeter ** 2))
-        if circularity > 0.1:
-            boxes1.append(cv2.boundingRect(c))
-
-    boxes2 = []
-    for c in contours2:
-        area = cv2.contourArea(c)
-        if area < 500:
-            continue
-        perimeter = cv2.arcLength(c, True)
-        if perimeter == 0:
-            continue
-        circularity = 4 * np.pi * (area / (perimeter ** 2))
-        if circularity > 0.7:
-            boxes2.append(cv2.boundingRect(c))
-
+def spojiDetekcije(boxes1, boxes2, boxes3):
     potvrdjeni = []
-    for boxA in boxes1:
-        for boxB in boxes2:
-            if iou(boxA, boxB) > 0.3:
-                potvrdjeni.append(boxB)
-                break
+    #za presek sva 3
+    # for boxA in boxes1:
+    #     for boxB in boxes2:
+    #         if iou2(boxA, boxB) < 0.3:
+    #             break
+    #         for boxC in boxes3:
+    #             if iou3(boxA, boxB, boxC) > 0.2:
+    #                 potvrdjeni.append(boxB)
+    #                 break
 
-    return potvrdjeni, boxes1, boxes2
+    #za A and (B or C)
+    for boxA in boxes1:
+        nadjeno = False
+        for boxB in boxes2:
+            if iou2(boxA, boxB) > 0.5:
+                potvrdjeni.append(min(boxA, boxB, key=povrsina))
+                nadjeno = True
+                break
+        if not nadjeno:
+            for boxC in boxes3:
+                if iou2(boxA, boxC) > 0.5:
+                    potvrdjeni.append(min(boxA, boxC, key=povrsina))
+                    break
+
+    return potvrdjeni
 
 
 def putBoundingBox(img, boxes, color):
@@ -121,7 +169,7 @@ while True:
     ret, img = cap.read()
     if not ret:
         break
-    corrected = gamma_correction(img, gamma=1.5)
+    corrected = gamma_correction(img, gamma=1)
     cv2.imshow("corrected", corrected)
 
     hsv = cv2.cvtColor(corrected, cv2.COLOR_BGR2HSV)
@@ -132,10 +180,11 @@ while True:
     bgr_clahe = cv2.cvtColor(hsv_clahe, cv2.COLOR_HSV2BGR)
     cv2.imshow("clahe", bgr_clahe)
 
-    contours1 = nadjiCrno(bgr_clahe)
-    contours2 = nadjiKonture(bgr_clahe)
+    boxes1 = nadjiCrno(bgr_clahe)
+    boxes2 = nadjiKonture(bgr_clahe)
+    boxes3 = nadjiKrugove(bgr_clahe)
 
-    potvrdjeneDetekcije, boxes1, boxes2 = spojiDetekcije(contours1, contours2)
+    potvrdjeneDetekcije = spojiDetekcije(boxes1, boxes2, boxes3)
 
     img_prve = img.copy()
     putBoundingBox(img_prve, boxes1, Blue)
@@ -144,6 +193,10 @@ while True:
     img_druge = img.copy()
     putBoundingBox(img_druge, boxes2, Red)
     cv2.imshow("druge", img_druge)
+
+    img_trece = img.copy()
+    putBoundingBox(img_trece, boxes3, Pink)
+    cv2.imshow("trece", img_trece)
 
     img_rezultat = img.copy()
     putBoundingBox(img_rezultat, potvrdjeneDetekcije, Green)
